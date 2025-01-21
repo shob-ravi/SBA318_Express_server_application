b73d219c14d525806fc61225e020ee24c065f332
Shobana Hariraman <shobanaravi2004@gmail.com>
2025-01-21 17:57:58 -0500

    added activity data

---
+++ data/activities.js
@@ -0,0 +1,24 @@
+const activities =
+[
+    {
+        name: "Mount Fuji Day Trip",
+        price: 120,
+        duration: "8 hours"
+      },
+      {
+        name: "Sushi Making Class",
+        price: 80,
+        duration: "2 hours"
+      },
+      {
+        name: "Seine River Cruise",
+        price: 50,
+        duration: "1 hour"
+      },
+      {
+        name: "Wine Tasting Tour",
+        price: 100,
+        duration: "3 hours"
+      }
+];
+module.exports = activities;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 3000;
 // Importing data from data files
 const destData = require("./data/destination");
 const hotelsData = require("./data/hotels");
+const activityData = require("./data/activities");
 
 // creating a get route for entire destination file
 app.get('/api/destination',(req,res)=>{
@@ -16,7 +17,9 @@ app.get('/api/destination',(req,res)=>{
 app.get('/api/hotels',(req,res)=>{
     res.json(hotelsData);
 })
-
+app.get('/api/activities',(req,res)=>{
+    res.json(activityData);
+})
 // starting the server and listening to some activity in port 3000
 app.listen(port, ()=>{
     console.log('server is running....');
